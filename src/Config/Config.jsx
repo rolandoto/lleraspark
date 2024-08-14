f73a723f075a4f42adebabcd01925faf5e439dbb
Jhonathan Velez <kokoa.arte@gmail.com>
2024-08-13 21:02:58 -0500

    initial commit update

--- src/Config/Config.jsx
+++ src/Config/Config.jsx
@@ -1,15 +1,20 @@
 export const config = {
-    serverRoute: "https://test-prueba-production.up.railway.app"
-    //serverRoute: "http://localhost:3001"
- }
- 
- export const SocketRoute = {
-    serverRoute: "https://railway.grupo-hoteles.com"
-    //serverRoute: "http://localhost:3001"
- }
- 
- export const CLoubedsRoute ={
-    serverRoute: "https://railway.grupo-hoteles.com"
-    //serverRoute:"http://localhost:3001"
- }
- 
+   serverRoute: "https://test-prueba-production.up.railway.app"
+   //serverRoute: "http://localhost:3001"
+}
+
+export const SocketRoute = {
+   serverRoute: "https://railway.grupo-hoteles.com"
+   //serverRoute: "http://localhost:3001"
+}
+
+export const CLoubedsRoute ={
+   serverRoute: "https://railway.grupo-hoteles.com"
+   //serverRoute:"http://localhost:3001"
+}
+
+
+export const Environment ={
+  propertyID:"315191",
+  Token:"cbat_SQeHkJAHRRdwp4bEtUx8muk97f34C5su"
+}
--- src/Page/Accomodation/Accomodation.jsx
+++ src/Page/Accomodation/Accomodation.jsx
@@ -24,6 +24,7 @@ import HeaderAccomodation from "../../Component/HeaderAccomodation/HeaderAccomod
 import Footer from "../../Component/Footer/Footer";
 import useRoomsPromotions from "../../Actions/useRoomsPromotions";
 import WhatsappButton from "../../Component/WhatsappButton/WhatsappButton";
+import { Environment } from "../../Config/Config";
 
 const Accommodation = () => {
 
@@ -61,10 +62,11 @@ const Accommodation = () => {
       
     
     const PostHotelByIdHotel = useCallback(async () => {
-        setContextMenuPosition(false);
-        setContextShowMenuPeople(false)
-        await getHotel({ id: 7, desde:formattedStartDate, hasta: formattedEndDate,counPeople:totalCountAdults });
-    }, [formattedStartDate,formattedEndDate,totalCountAdults]);
+      setContextMenuPosition(false);
+      setContextShowMenuPeople(false)
+      await getHotel({propertyID:Environment.propertyID,startDate:formattedStartDate, endDate: formattedEndDate,token:Environment.Token,counPeople:totalCountAdults });
+  }, [formattedStartDate,formattedEndDate,totalCountAdults]);
+
 
     useEffect(() =>{
       PostHotelByIdHotel()
@@ -113,12 +115,16 @@ const Accommodation = () => {
                 </div> 
        ) 
       }if(error){
-        return    <EmpyCart title={"No tenemos habitaciones disponibles para esta ocupación"} />
+        return   <EmpyCart title={"Lo sentimos, en este momento no contamos con alojamientos disponibles para las fechas seleccionadas. Por favor, modifique las fechas para generar una nueva búsqueda. O puede contactar a nuestro equipo para obtener más información llamando al +57 3052897766."} />
                 }
-        return <>  {hotel?.availableRooms?.map((List,index) => <CardAccomodation  
-                                                                  promotion={promotion}   
-                                                                  totalCountAdults={totalCountAdults}
-                                                                  key={index} {...List}/>)}</>
+        return <>  {hotel?.data?.map((List,index) => <CardAccomodation  
+                                                        counPeople={hotel.counPeople}
+                                                        endDate={hotel.endDate}
+                                                        startDate={hotel.startDate}
+                                                        nightsToday={hotel.nights}
+                                                        promotion={promotion} 
+                                                        totalCountAdults={totalCountAdults}
+                                                        key={index} {...List}/>)}</>
     }
     const monthsToShow = window.innerWidth >= 700 ? 2 : 1;
 
